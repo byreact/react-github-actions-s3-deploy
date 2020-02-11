4c443f2ae7d276d8c66354be25d881a8c7e52f73
임경민 <ljlm0402@naver.com>
2020-02-11 22:58:20 +0900

    Fix package.json

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>React + TypeScript + Github Actions + S3 Build</p>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
